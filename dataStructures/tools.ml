b80513bd7ed710c881e2bc75e9f778ae6386bb7c
Hector Urbina <ajendrex@gmail.com>
2012-03-07 18:38:04 -0300

    Tools module commented

--- dataStructures/tools.ml
+++ dataStructures/tools.ml
@@ -1,20 +1,33 @@
+(** Implements several frequently used functions. *)
+
+(** Type to describe a point in a source file. *)
 type pos = string*int*int
 
+(** Returns the second element of a tuple of 3 elements. For values of type {!pos}, it correspond to the line number in source file. *)
 let ln (_,i,_) = i
+(** Returns the third element of a tuple of 3 elements. For values of type {!pos}, it correspond to the column number in source file. *)
 let cn (_,_,j) = j
+(** Returns the first element of a tuple of 3 elments. For values of type {!pos}, it correspond to the source-file name. *)
 let fn (n,_,_) = n
-
+(** Defines a null {!pos}. *)
 let no_pos = ("",-1,-1)
 
+(** Returns a string representation of a {!pos} value. *)
 let string_of_pos = fun (n,i,j) -> ("(in "^n^") line "^(string_of_int i)^", char "^(string_of_int j)^": ")
-
+(** Prints the elements of a set, separated by commas.
+    @param f Function used to build a string representation of each element in the set.
+    @param fold Function used to reduce the set.
+    @param set A set. *)
 let string_of_set f fold set = 
   let l = 
     fold (fun i cont -> (f i)::cont) set [] 
   in
     Printf.sprintf "{%s}" (String.concat "," l)
-
-
+(** Prints the entries of a map, separated by commas.
+    @param f1 Function used to build a string representation of each entry's key.
+    @param f2 Function used to build a string representation of each entry's value.
+    @param swap If true, swap keys and values of the map. Optional, default is false.
+    @param map A map. *)
 let string_of_map ?(swap=false) f1 f2 fold map = 
   let l = 
     fold (fun i j cont -> 
@@ -22,12 +35,16 @@ let string_of_map ?(swap=false) f1 f2 fold map =
 			else ((f1 i)^"->"^(f2 j))::cont) map [] 
   in
     Printf.sprintf "[%s]" (String.concat "," l)
-
+(** Returns a string representation of an array.
+    @param f Function used to get a string representation of each element in the array. 
+    @param ar An array. *)
 let string_of_array f ar =
 	let l = ref [] in 
 		Array.iteri (fun i e -> l:=(((string_of_int i)^":"^(f e))::!l)) ar ;
 		"[|"^(String.concat ";" (List.rev !l))^"|]"
-		
+(** Returns a string representation of a list.
+    @param f Function used to get a string representation of each element in the list.
+    @param l A list. *)		
 let string_of_list f l =
 	"["^(String.concat ";" (List.map f l))^"]"
 	
@@ -39,7 +56,8 @@ let pow x n =
 	in
 	aux x n 1
 
-(*number of bits used to represent n in base 2*)
+(** Returns the number of bits needed to represent n in base 2.
+    @param n An integer. *)
 let bit_rep_size n = 
 	let rec aux p acc = 
 		if p = 0 then acc
@@ -49,11 +67,14 @@ let bit_rep_size n =
 	in
 		aux n 0  
 
+(** Replace spaces by underscores in a given string and returns it.
+    @param str A string. *)
 let replace_space str = 
 	let cpt = ref 0 in
 	String.iter (fun c -> if c=' ' then String.set str !cpt '_' ; cpt := !cpt+1) str ;
 	str
 
+(** Reads standard input and returns it without spaces. *)
 let read_input () = 
 	let rec parse acc input =
 		match Stream.next input with

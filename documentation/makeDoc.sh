0c40e696ee0643c8277ab667179b10695a9cd2d7
Hector Urbina <ajendrex@gmail.com>
2012-03-08 15:34:56 -0300

    automatic docs compilation in documentation folder

---
+++ documentation/makeDoc.sh
@@ -0,0 +1,1 @@
+ocamldoc -hide-warnings -html -I ../cflow -I ../dataStructures -I ../Error -I ../grammar -I ../main -I ../models -I ../pattern -I ../simulation -I ../siteGraphs `find ../ -name *.ml`
